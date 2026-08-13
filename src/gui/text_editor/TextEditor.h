a8c69d02cc2e98d168270649b9b1cddef7b9bf77
Kenenth Morse <kenmorse2@proton.me>
2026-08-13 13:28:42 -0600

    Add TextEditor class for taking notes during a session.

---
+++ src/gui/text_editor/TextEditor.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE namespace Ui {
+    class TextEditor;
+}
+
+QT_END_NAMESPACE
+
+class TextEditor : public QWidget
+{
+    Q_OBJECT public:
+    explicit TextEditor(QWidget *parent = nullptr);
+
+    ~TextEditor() override;
+
+private slots:
+    void applyHeading(int index);
+
+    void toggleBold();
+
+    void toggleItalic();
+
+    void toggleStrikethrough();
+
+    void addLink();
+
+    void toggleBulletList();
+
+    void updateToolbarState();
+
+private:
+    [[nodiscard]] int currentHeadingLevel() const;
+
+    [[nodiscard]] qreal headingFontSize(int level) const;
+
+    Ui::TextEditor *ui{};
+};
